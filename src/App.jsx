16c6dfa865489a72d46fecd3ade567f52a58494d
Nicholas Zhang <nzhan01@bu.edu>
2025-03-18 17:07:21 -0400

    inital commit with personal website

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+
+import './components/navbar/navbar.css'
+import Navbar from './components/navbar/navbar.jsx'
+import './App.css'
+import Profile from "./components/profile/profile.jsx";
+import './components/profile/profile.css'
+import Projects from './components/projects/projects.jsx';
+import './components/projects/projects.css';
+import Experiences from './components/experiences/experiences.jsx'
+import About from './components/about/about.jsx'
+
+function App() {
+    return (
+        
+        <>
+            <Navbar/>
+            <Profile/>
+            <About/>
+            <div id ="projects">
+                <h2>Projects</h2>
+                <Projects name ="Valentines Card" description ="Interactive valentines card" github="https://github.com/nzhan01/JDT-spring-25-alpha/tree/main/workspace/valentines"/>
+                <Projects name ="Movie_Max App" description ="Interactive movie watchlist maker" github="https://github.com/nzhan01/movie_max_app/tree/final"/>
+
+            </div>
+
+            <div id="exp">
+                <h2>Work Experience</h2>
+                <Experiences
+                    title = "NS201 Biology Teaching Assistant"
+                    info = "Boston University, Boston, MA"
+                    dates = "5/2024 - Present"
+                    bullets={[
+                        "Collaborated with five professors to design, implement, instruct, and evaluate laboratory sessions",
+                        "Resided over multiple weekly biology lab activities for over 60 students",
+                        "Tutored students during weekly office hours"]}/>
+                <Experiences
+                    title = "ASL Language Link Facilitator"
+                    info = "Boston University Wheelock College, Boston, MA"
+                    dates = "2/2025 - Present"
+                    bullets={[
+                        "Worked with two Deaf Studies professors to develop weekly lesson plans",
+                        "Lead weekly tutoring sessions in American Sign Language"]}/>
+            </div>
+
+        </>
+    )
+}
+
+export default App
